4884da172d7d9e98ba93c4e354656b8ecca48818
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2022-07-11 00:05:04 +0100

    Delete bin/utils directory

--- bin/utils/KeyConstants.js
+++
@@ -1,9 +0,0 @@
-export var KeyConstants = {
-    pressLeft: 'pressLeft',
-    releaseLeft: 'releaseLeft',
-    pressRight: 'pressRight',
-    releaseRight: 'releaseRight',
-    pressUp: 'pressUp',
-    releaseUp: 'releaseUp'
-};
-//# sourceMappingURL=KeyConstants.js.map
